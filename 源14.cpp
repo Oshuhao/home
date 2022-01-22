f2d371a5d619ae26f00a3b9a0fc939937af5eb80
HUHAO <98003327+Oshuhao@users.noreply.github.com>
2022-01-22 16:46:38 +0800

    Add files via upload

---
+++ 源14.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main() {
+	
+	int a = 10;
+	a = 100;
+	cout << "a=" << a<< endl;
+	a = 10;
+	a += 1000;
+	cout << "a=" << a << endl;
+	a = 10;
+	a -= 100000;
+	cout << "a=" << a << endl;
+	a = 10;
+	a *= 100000;
+	cout << "a=" << a << endl;
+   a = 10; 
+   a /= 5;
+   cout << "a=" << a << endl;
+   a = 10;
+   a %= 3;
+	cout << "a=" << a << endl;
+	
+
+	
+
+
+
+
+
+
+
+	system("pause");
+	
+	return 0;
+}
