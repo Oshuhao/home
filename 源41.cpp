a4cd53fe67fd770ee94aac89ab46baa0361090c6
HUHAO <98003327+Oshuhao@users.noreply.github.com>
2022-01-26 19:26:41 +0800

    Add files via upload

---
+++ 源41.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include "swap.h";
+
+//void swap(int a, int b)
+//{
+
+	//int temp = a;
+//	a = b;
+//	b = temp;
+//	cout << "a=" << a << endl;
+//	cout << "b=" << b << endl;
+//}
+
+
+int main() { 
+
+	int a = 10;
+	int b = 20;
+
+	//cout << max(a, b) << endl;
+	swap(a, b);
+
+	
+	
+	
+	system ("pause");
+	return 0;
+}
