ea31692b3419e04e2b584c56853a19802dd834b8
HUHAO <98003327+Oshuhao@users.noreply.github.com>
2022-01-27 19:09:37 +0800

    Add files via upload

---
+++ 源42.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a = 10;
+	int* p;
+	p = &a;
+	cout << "a的地址为：" << &a << endl;
+	cout << "指针p为：" << p << endl;
+	*p;
+	cout << "a=" << a << endl;
+	cout << "*p=" << *p << endl;
+	
+	
+	
+	
+	system("pause");
+	return 0;
+}
