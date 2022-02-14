3467e5ba9e12d0a1af03c4a62ad5648d1ea622fa
HUHAO <98003327+Oshuhao@users.noreply.github.com>
2022-02-14 17:57:33 +0800

    Add files via upload

---
+++ 源54.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int func(int a, int  b, int c)
+{
+	return a * b * c;
+
+}
+int func2(int d, int e, int f)
+{
+	return d + e + c;
+}
+
+}
+int main ()
+{
+	
+	cout << func(10, 20, 30) << endl;
+	cout << func2(5200000, 1300, 14) << endl;
+	
+	
+	system("pause");
+	return 0;
+}
+
+
+
